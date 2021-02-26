e83ef27531092504dfbcf5a3deb13fd840d4a15a
Nick Avgerinos <nicka@axcella.com>
2021-02-26 01:20:10 -0800

    don't call the CMSPlugin.page property

--- aldryn_bootstrap3/model_fields.py
+++ aldryn_bootstrap3/model_fields.py
@@ -166,9 +166,38 @@ class LinkMixin(models.Model):
             ref_page = self.link_page
             link = ref_page.get_absolute_url()
 
-            if ref_page.node.site_id != getattr(self.page.node, 'site_id', None):
-                ref_site = Site.objects._get_site_by_id(ref_page.site_id)
-                link = '//{}{}'.format(ref_site.domain, link)
+            # simulate the call to the unauthorized CMSPlugin.page property
+            cms_page = self.placeholder.page if self.placeholder_id else None
+
+            # first, we check if the placeholder the plugin is attached to
+            # has a page. Thus the check "is not None":
+            if cms_page is not None:
+                if getattr(cms_page, 'node', None):
+                    cms_page_site_id = getattr(cms_page.node, 'site_id', None)
+                else:
+                    cms_page_site_id = getattr(cms_page, 'site_id', None)
+            # a plugin might not be attached to a page and thus has no site
+            # associated with it. This also applies to plugins inside
+            # static placeholders
+            else:
+                cms_page_site_id = None
+
+            # now we do the same for the reference page the plugin links to
+            # in order to compare them later
+            if cms_page is not None:
+                if getattr(cms_page, 'node', None):
+                    ref_page_site_id = ref_page.node.site_id
+                else:
+                    ref_page_site_id = ref_page.site_id
+            # if no external reference is found the plugin links to the
+            # current page
+            else:
+                ref_page_site_id = Site.objects.get_current().pk
+
+            if ref_page_site_id != cms_page_site_id:
+                ref_site = Site.objects._get_site_by_id(ref_page_site_id).domain
+                link = '//{}{}'.format(ref_site, link)
+
         elif self.link_url:
             link = self.link_url
         elif self.link_phone:
